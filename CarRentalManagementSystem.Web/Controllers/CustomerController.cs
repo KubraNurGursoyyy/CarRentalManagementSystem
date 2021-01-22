35448134b1eedb75fc58ac85909a34715de44837
Bayram Vefa AKSOY <bayramvefa.aksoy@gmail.com>
2021-01-22 16:35:24 +0300

    Proje dosyası ekle.

---
+++ CarRentalManagementSystem.Web/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using CarRental.Commons.Concretes.Helper;
+using CarRental.Commons.Concretes.Logger;
+using CarRentalManagementSystem.Web.CustomerWebService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace CarRentalManagementSystem.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+
+        public ActionResult LogInCustomer()
+        {
+            string ViewName = "CustomerView";
+            try
+            {
+                string username = TempData["username"].ToString();
+                string userpassword = TempData["password"].ToString();
+                if (LoginCustomer(username, userpassword))
+                {
+                    FormsAuthentication.SetAuthCookie(username, true);
+                    return RedirectToAction(ViewName);
+                }
+                else
+                {
+                    return RedirectToAction("LogInView", "LogInAndSignUp");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("CustomerController::LogInCustomer::Error occured.", ex);
+            }
+        }
+        public ViewResult SignUpAsCustomerView()
+        {
+            return View();
+        }
+        public ViewResult CustomerView()
+        {
+            return View();
+        }
+        #region PRIVATE METHODS
+        private bool LoginCustomer(string username, string password)
+        {
+            try
+            {
+                using (var customerSoapClient= new CustomerWebServiceSoapClient())
+                {
+                    return customerSoapClient.LogInAsCustomer(username, password);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("CustomerController::LogInCustomer::Error occured.", ex); 
+            }
+        }
+        #endregion
+    }
+}
---
+++ CarRentalManagementSystem.Web/Controllers/LogInAndSignUpController.cs
@@ -0,0 +1,75 @@
+﻿using CarRental.Commons.Concretes.Helper;
+using CarRental.Commons.Concretes.Logger;
+using System;
+using System.Web.Mvc;
+
+namespace CarRentalManagementSystem.Web.Controllers
+{
+    public class LogInAndSignUpController : Controller
+    {
+        // GET: LogInAndSignUp
+
+        [HttpGet]
+        public ViewResult SignUpAs()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult LogInView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogInView(FormCollection collection)
+        {
+            try
+            {
+                TempData["username"] = collection["username"];
+                TempData["password"] = collection["userpassword"];
+                switch (collection["Login"])
+                {
+                    case "Kaydol":
+                        return RedirectToAction("SignUpAs");
+                    case "Kullanıcı Girişi":
+                        return RedirectToAction("LogInCustomer", "Customer");
+                    case "Çalışan Girişi":
+                        ViewBag.Message = "The operation was cancelled!";
+                        return View();
+                    case "Şirket Girişi":
+                        ViewBag.Message = "The operation was cancelled!";
+                        return View();
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("LogInAndSignUpController::LogInView::Error occured.", ex);
+            }
+            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult SignUpAs(FormCollection collection)
+        {
+            try
+            {
+                switch (collection["SignUpAs"])
+                {
+                    case "Kullanıcı":
+                        return RedirectToAction("SignUpAsCustomer", "Customer");
+                    case "Şirket":
+                        return RedirectToAction("SignUpAsCompany", "Company");
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("LogInAndSignUpController::SignUpAs::Error occured.", ex);
+            }   
+        }
+    }
+}
